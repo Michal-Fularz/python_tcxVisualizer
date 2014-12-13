41c2e3ca2e522fc6d629e7198d6927684eac1769
Michal Fularz <michal.fularz@gmail.com>
2014-12-13 19:13:49 +0100

    Used https://github.com/vkurup/python-tcxparser project for reading the tcx file, added code for copying elements to list.

--- python_tcxVisualizer/python_tcxVisualizer.py
+++ python_tcxVisualizer/python_tcxVisualizer.py
@@ -1,5 +1,34 @@
-from lxml import etree
+from lxml import objectify
+from tcxparser import TCXParser
 
-root = etree.Element("root")
+namespace = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
+
+tcx = TCXParser("F:/Projects/VS_2013/python_tcxVisualizer/data/endomondo_altitude_test.tcx")
+print(tcx.latitude)
+latitudeList = []
+for trackpoint in tcx.Trackpoints:
+    #print(trackpoint.Position.LatitudeDegrees.pyval)
+    latitudeList.append(trackpoint.Position.LatitudeDegrees.pyval)
+print(latitudeList)
+
+
+
+# http://infohost.nmt.edu/tcc/help/pubs/pylxml/web/index.html
+object = objectify.parse("F:/Projects/VS_2013/python_tcxVisualizer/data/endomondo_altitude_test.tcx")
+print(object)
+root = object.getroot()
+print(root)
+activity = root.Activities.Activity
+print(activity)
+
+activity_type = activity.attrib['Sport'].lower()
+print(activity_type)
+
+latitude = activity.Lap.Track.Trackpoint[200].Position.LatitudeDegrees.pyval
+print(latitude)
+
+
+
+
 
 print('Hello World')
